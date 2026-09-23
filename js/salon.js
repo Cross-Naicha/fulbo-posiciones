c722112422f5df5603bdc8ec7f7ca99cc0fb771d
Daruich, Nicolas Alberto <nic.aldar@hotmail.com>
2026-09-23 09:08:31 -0300

    Chiqui-maña

--- js/salon.js
+++ js/salon.js
@@ -67,7 +67,7 @@ const SALON_DE_LA_FAMA_DATA = {
           prioridad: 3
         },
         {
-          titulo: "??? | SuperCopa Invernal 2026",
+          titulo: "CHIQUI | SuperCopa Invernal 2026",
           competencia: "SuperCopa",
           prioridad: 3
         },
